9e79abe2023e07b65e3a3a619e98d990335b5dbf
ebpacheco <ebertoncinip@live.com>
2023-06-12 19:59:45 -0300

    Add cozinha e restaurante repository

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Cozinha> todas() {
+		TypedQuery<Cozinha> query = manager.createQuery("FROM Cozinha", Cozinha.class);
+		return query.getResultList();
+//		return manager.createQuery("FROM cozinha", Cozinha.class).getResultList();
+	}
+
+	@Override
+	public Cozinha porId(Long id) {
+		return manager.find(Cozinha.class, id);
+	}
+
+	@Transactional
+	@Override
+	public Cozinha adicionar(Cozinha cozinha) {
+		return manager.merge(cozinha);
+	}
+
+	@Transactional
+	@Override
+	public void remover(Cozinha cozinha) {
+		cozinha = porId(cozinha.getId());
+		manager.remove(cozinha);
+	}
+
+}
