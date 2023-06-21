12fb9b0a0dc2db286650614b4527a17dc3d84123
ebpacheco <eduardobertoncinip@live.com>
2023-06-21 20:02:00 -0300

    Add post cozinha

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@JsonRootName("cozinha")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 @Entity
