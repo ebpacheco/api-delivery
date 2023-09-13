17d571acaf803416332993a66a062c9820ae07d6
ebpacheco <eduardobertoncinip@live.com>
2023-09-13 17:43:32 -0300

    Refatorando excecoes

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 
@@ -18,6 +19,12 @@ public class ApiExceptionHandler {
 	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
 		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	}
+
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+		Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
 	}
 
 	@ExceptionHandler(NegocioException.class)
--- src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
@@ -1,9 +1,7 @@
 package com.algaworks.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+//O HttpStatus está sendo tratado no ApiExcpetionController
+//@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class NegocioException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
