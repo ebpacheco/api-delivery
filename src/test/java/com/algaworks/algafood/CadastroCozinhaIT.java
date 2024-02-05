fa1d331bf3513d66a136c28ca6c9eee0032f1870
ebpacheco <eduardobertoncinip@live.com>
2024-02-05 20:15:15 -0300

    Configurando setUp e criando banco para test

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -3,18 +3,34 @@ package com.algaworks.algafood;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 
 	@LocalServerPort
 	private int port;
+
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+//	@Autowired
+//	private Flyway flyway;
 
 	@BeforeEach
 	public void setUp() {
@@ -22,20 +38,36 @@ public class CadastroCozinhaIT {
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
 
+		databaseCleaner.clearTables();
+		preparaDados();
+
+//		flyway.migrate();
 	}
 
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-		RestAssured.given().accept(ContentType.JSON)
-				.when().get()
-				.then().statusCode(HttpStatus.OK.value());
+		RestAssured.given().accept(ContentType.JSON).when().get().then().statusCode(HttpStatus.OK.value());
 	}
 
 	@Test
-	public void deveRetornar4Cozinhas_QuandoConsultarCozinhas() {
-		RestAssured.given().accept(ContentType.JSON)
-				.when().get()
-				.then().body("id", Matchers.hasSize(4))
-				.body("nome", Matchers.hasItems("Indiana", "Brasileira"));
+	public void deveRetornar2Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.given().accept(ContentType.JSON).when().get().then().body("id", Matchers.hasSize(2)).body("nome",
+				Matchers.hasItems("Indiana", "Brasileira"));
+	}
+
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given().body("{ \"nome\" : \"Chinesa\" }").contentType(ContentType.JSON).accept(ContentType.JSON)
+				.when().post().then().statusCode(HttpStatus.CREATED.value());
+	}
+
+	private void preparaDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Indiana");
+		cozinhaRepository.save(cozinha1);
+
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Brasileira");
+		cozinhaRepository.save(cozinha2);
 	}
 }
