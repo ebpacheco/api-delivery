ba78b88efa861c0c49cdda34ab5d34e9e61750fe
ebpacheco <eduardobertoncinip@live.com>
2024-06-12 21:50:44 -0300

    Refatorando endpoints de transicao de status

--- src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -2,8 +2,8 @@ package com.algaworks.algafood.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +17,21 @@ public class FluxoPedidoController {
 	@Autowired
 	private FluxoPedidoService fluxoPedidoService;
 
-	@GetMapping(value = "/confirmacao")
+	@PutMapping(value = "/confirmacao")
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void confirmar(@PathVariable Long pedidoId) {
 		fluxoPedidoService.confirmar(pedidoId);
+	}
+
+	@PutMapping(value = "/cancelamento")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void cancelar(@PathVariable Long pedidoId) {
+		fluxoPedidoService.cancelar(pedidoId);
+	}
+
+	@PutMapping(value = "/entrega")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void entregar(@PathVariable Long pedidoId) {
+		fluxoPedidoService.entregar(pedidoId);
 	}
 }
