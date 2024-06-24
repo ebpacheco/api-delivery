6fb883388e94216a3251fce6e23e78e13e381d05
ebpacheco <eduardobertoncinip@live.com>
2024-06-24 18:31:35 -0300

    Refatorando regras para transicao de status de pedido

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.algaworks.algafood.domain.exception.NegocioException;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -83,5 +85,28 @@ public class Pedido {
 //	public void atribuirPedidoAosItens() {
 //		getItens().forEach(item -> item.setPedido(this));
 //	}
+
+	public void confirmar() {
+		setStatus(StatusPedido.CONFIRMADO);
+		setDataConfirmacao(OffsetDateTime.now());
+	}
+
+	public void entregar() {
+		setStatus(StatusPedido.ENTREGUE);
+		setDataEntrega(OffsetDateTime.now());
+	}
+
+	public void cancelar() {
+		setStatus(StatusPedido.CANCELADO);
+		setDataCancelamento(OffsetDateTime.now());
+	}
+
+	private void setStatus(StatusPedido novoStatus) {
+		if (getStatus().naoPodeAlterarPara(novoStatus)) {
+			throw new NegocioException(String.format("Status do pedido %d nao pode ser alterado de %s para %s", getId(),
+					getStatus().getDescricao(), novoStatus.getDescricao()));
+		}
+		this.status = novoStatus;
+	}
 
 }
