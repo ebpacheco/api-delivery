026f302f2c6c98fc925e2c4afb6ea9a2a0e49cbb
ebpacheco <eduardobertoncinip@live.com>
2024-08-06 20:19:00 -0300

    Estruturando endpoint e servico para emissao de relatorio PDF

---
+++ src/main/java/com/algaworks/algafood/domain/service/VendaReportService.java
@@ -0,0 +1,8 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+
+public interface VendaReportService {
+
+	public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffSet);
+}
