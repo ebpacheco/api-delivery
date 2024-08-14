6ca4b3e416d137270eda60660384f09f35f33c3c
ebpacheco <eduardobertoncinip@live.com>
2024-08-14 12:06:39 -0300

    Implementando servico de cadastro de foto produto

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteFotoProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteFotoProdutoController.java
@@ -1,15 +1,20 @@
 package com.algaworks.algafood.api.controller;
 
-import java.nio.file.Path;
-import java.util.UUID;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.algafood.api.assembler.FotoProdutoDTOAssembler;
+import com.algaworks.algafood.api.model.FotoProdutoDTO;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
+import com.algaworks.algafood.domain.service.CatalogoFotoProdutoService;
 
 import jakarta.validation.Valid;
 
@@ -17,19 +22,39 @@ import jakarta.validation.Valid;
 @RequestMapping(value = "/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteFotoProdutoController {
 
+	@Autowired
+	private CatalogoFotoProdutoService catalogoFotoProdutoService;
+
+	@Autowired
+	private CadastroProdutoService cadastroProdutoService;
+
+	@Autowired
+	private FotoProdutoDTOAssembler fotoProdutoDTOAssembler;
+
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+	public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
 			@Valid FotoProdutoInput fotoProdutoInput) {
+//		var nomeArquivo = UUID.randomUUID() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+//		var arquivoFoto = Path.of("C:\\Users\\DudatraK\\OneDrive\\Documentos\\projeto-algafood-fotos\\catalogo",
+//				nomeArquivo);
+//		try {
+//			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
 
-		var nomeArquivo = UUID.randomUUID() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
-		var arquivoFoto = Path.of("C:\\Users\\DudatraK\\OneDrive\\Documentos\\projeto-algafood-fotos\\catalogo",
-				nomeArquivo);
+		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
+		MultipartFile arquivo = fotoProdutoInput.getArquivo();
 
-		try {
-			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		FotoProduto foto = new FotoProduto();
+		foto.setProduto(produto);
+		foto.setDescricao(fotoProdutoInput.getDescricao());
+		foto.setContentType(arquivo.getContentType());
+		foto.setTamanho(arquivo.getSize());
+		foto.setNomeArquivo(arquivo.getOriginalFilename());
+		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto);
+		return fotoProdutoDTOAssembler.toDTO(fotoSalva);
+
 	}
 
 }
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/ProdutoRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.repository.ProdutoRepositoryQueries;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Repository
+public class ProdutoRepositoryImpl implements ProdutoRepositoryQueries {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Transactional
+	@Override
+	public FotoProduto save(FotoProduto foto) {
+		return manager.merge(foto);
+	}
+
+}
