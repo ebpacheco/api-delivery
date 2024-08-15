99e5d1f076197b8b7e5c3b0bd5735a0ca5cad133
ebpacheco <eduardobertoncinip@live.com>
2024-08-15 11:36:57 -0300

    Implementando servico de armazenamento de foto local

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteFotoProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteFotoProdutoController.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +35,7 @@ public class RestauranteFotoProdutoController {
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
-			@Valid FotoProdutoInput fotoProdutoInput) {
+			@Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 
 		Produto produto = cadastroProdutoService.buscarOuFalhar(restauranteId, produtoId);
 		MultipartFile arquivo = fotoProdutoInput.getArquivo();
@@ -44,7 +46,7 @@ public class RestauranteFotoProdutoController {
 		foto.setContentType(arquivo.getContentType());
 		foto.setTamanho(arquivo.getSize());
 		foto.setNomeArquivo(arquivo.getOriginalFilename());
-		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto);
+		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto, arquivo.getInputStream());
 		return fotoProdutoDTOAssembler.toDTO(fotoSalva);
 
 	}
