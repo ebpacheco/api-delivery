36977701176387ed1cfa2740c8610a672bfc4d42
ebpacheco <eduardobertoncinip@live.com>
2024-12-18 11:29:18 -0300

    Implementando servico de envio fake e sandbox

--- src/main/java/com/algaworks/algafood/core/validation/EmailConfig.java
+++ src/main/java/com/algaworks/algafood/core/validation/EmailConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
 import com.algaworks.algafood.infrastructure.service.email.FakeEnvioEmailService;
+import com.algaworks.algafood.infrastructure.service.email.SandBoxEnvioEmailService;
 import com.algaworks.algafood.infrastructure.service.email.SmtpEnvioEmailService;
 
 @Configuration
@@ -16,13 +17,15 @@ public class EmailConfig {
 	private EmailProperties emailProperties;
 
 	@Bean
-	public EnvioEmailService envioEmailService() {
+	EnvioEmailService envioEmailService() {
 		// Acho melhor usar switch aqui do que if/else if
 		switch (emailProperties.getImpl()) {
 		case FAKE:
 			return new FakeEnvioEmailService();
 		case SMTP:
 			return new SmtpEnvioEmailService();
+		case SANDBOX:
+			return new SandBoxEnvioEmailService();
 		default:
 			return null;
 		}
