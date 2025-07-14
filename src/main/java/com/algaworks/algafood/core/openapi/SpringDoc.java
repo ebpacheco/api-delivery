5a7fb1a44cc617d1dcbb5fa573d6176a3b1dfae1
ebpacheco <eduardobertoncinip@live.com>
2025-07-14 15:16:56 -0300

    Gerando a documentacao da API em HTML com Swagger-ui

---
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringDoc.java
@@ -0,0 +1,38 @@
+package com.algaworks.algafood.core.openapi;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+@OpenAPIDefinition
+public class SpringDoc implements WebMvcConfigurer {
+
+	@Bean
+	GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder().group("public").packagesToScan("com.algaworks.algafood.api").build();
+	}
+
+	@Bean
+	OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("SpringShop API").description("Spring shop sample application").version("v0.0.1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.externalDocs(new ExternalDocumentation().description("SpringShop Wiki Documentation")
+						.url("https://springshop.wiki.github.org/docs"));
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
