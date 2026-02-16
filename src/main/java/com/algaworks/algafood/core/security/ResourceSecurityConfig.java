b898b1eb979eee71712ca64ca862e6d12069c113
ebpacheco <eduardobertoncinip@live.com>
2026-02-16 10:46:58 -0300

    Configurando o Resource Server com a nova stack do Spring Security

---
+++ src/main/java/com/algaworks/algafood/core/security/ResourceSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceSecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+				.oauth2ResourceServer(oauth2 -> oauth2.opaqueToken(Customizer.withDefaults()));
+
+		return http.build();
+	}
+}
